c5ca1bb73370accb3141a1162b7377fb20730357
Anatolii Shcherbyna <anatoliiivshcherbyna@gmail.com>
2024-11-28 19:27:09 +0200

    2

---
+++ src/db/models/contact.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const ContactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      reqired: true,
+    },
+    isFavourite: { type: Boolean, default: false },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const ContactsColection = model('contacts', ContactSchema);
---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import pinoHttp from 'pino-http';
+import pino from 'pino';
+import cors from 'cors';
+
+import ContactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = process.env.PORT || 3000;
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+
+  const logger = pino({
+    level: process.env.LOG_LEVEL || 'info',
+    transport: { target: 'pino-pretty' },
+  });
+
+  app.use(cors());
+  app.use(pinoHttp({ logger }));
+
+  app.use(ContactsRouter);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+};
